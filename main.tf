1e2b3ca8dc306b106b435e92f94bb0a5dd7cf412
Kerston2104 <113289902+Kerston2104@users.noreply.github.com>
2025-10-26 22:41:53 +0530

    Made a new feature

--- main.tf
+++ main.tf
@@ -7,14 +7,14 @@ resource "random_id" "id" { byte_length = 6 }
 # 1. A Resource Group (a folder for all our stuff)
 resource "azurerm_resource_group" "rg" {
   name     = "DemoDevOps-RG"
-  location = "East US"
+  location = "Central India" # <-- CORRECTED REGION
 }
 
 # 2. The Azure Container Registry (to store our Docker image)
 resource "azurerm_container_registry" "acr" {
   name                = "demoacr${random_id.id.hex}"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  location            = azurerm_resource_group.rg.location # Inherits location from RG
   sku                 = "Basic"
   admin_enabled       = true
 }
@@ -23,23 +23,25 @@ resource "azurerm_container_registry" "acr" {
 resource "azurerm_service_plan" "plan" {
   name                = "demo-devops-plan"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  location            = azurerm_resource_group.rg.location # Inherits location from RG
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "B1" # Basic tier, should be free or cheap
 }
 
 # 4. The App Service (the "server" that runs the container)
 resource "azurerm_linux_web_app" "webapp" {
   name                = "demo-webapp-${random_id.id.hex}"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_service_plan.plan.location
+  location            = azurerm_service_plan.plan.location # Inherits location from Plan
   service_plan_id     = azurerm_service_plan.plan.id
   
-site_config {
-    # This block can be empty
-  }
-
+  # Correct placement for app_settings
   app_settings = {
     "WEBSITES_PORT" = "8080" # Tell Azure our app uses port 8080
   }
+
+  site_config {
+    # This block can be empty or have other settings, but NOT app_settings
+  }
 }
+
